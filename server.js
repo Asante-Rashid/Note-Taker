e88000dc451e91433773b3c42a2c723a85fd3129
Asante <asanterashid.biz@gmail.com>
2021-03-11 19:15:07 -0500

    connected express server to HTML routes

--- server.js
+++ server.js
@@ -4,14 +4,23 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // HTML Routes
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 //API Routes
 
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
-  });
+});
